2e3550ffcd07badcdc583df75241a45a10454e92
mohammadqasimmatloob <Qasimmatloob786@gmail.com>
2024-04-19 09:11:11 +0100

    update the nav

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -2,7 +2,7 @@
 <div id="main-nav">
 
    <a href="modules.php">My Modules</a> |
-   <a href="student.php">Student</a> |
+   <a href="student.php">Student Details</a> |
    <a href="AddStudent.php">ADD New Student</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
